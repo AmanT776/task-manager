915fb8532870d1b71745daf2ae7528ac40d71899
AmanT776 <amantes776@gmail.com>
2025-07-02 16:16:22 +0300

    feat: added protected route

--- backend/routes/taskRoutes.js
+++ backend/routes/taskRoutes.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const router = express.Router();
-const {addTask,getAllTasks,getTask} = require("../routes");
+const {addTask,getAllTasks,getTask} = require("../controllers/taskController");
+const {protect, authorize } = require("../middlewares/auth");
 
-router.post('/addTask',addTask);
-router.get('/getTasks',getAllTasks);
-router.get('/getTask',getTask);
+router.post('/addTask',protect,addTask);
+router.get('/getTasks',protect,authorize["admin"],getAllTasks);
+router.get('/getTask',protect,getTask);
 
 module.exports = router;
